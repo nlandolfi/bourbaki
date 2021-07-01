93ed0ee319208ee2c3a1bdfefbf7ac8ca8d512a2
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2021-07-01 10:46:25 -0700

    checkpoint

--- bbk/cmd/sheet/bbk_sheet.go
+++ bbk/cmd/sheet/bbk_sheet.go
@@ -80,7 +80,7 @@ func writeFile(all map[string]*bbk.ParseResult, p *bbk.ParseResult) {
 	fmt.Fprintln(os.Stdout, "\\input{./macros.tex}")
 	fmt.Fprintln(os.Stdout, "\\sstart")
 	fmt.Fprintln(os.Stdout, "\\bpage\\clearpage")
-	fmt.Fprintf(os.Stdout, "\\stitle{%s}\n", bbk.Title(p.Name))
+	fmt.Fprintf(os.Stdout, "\\stitle{%s}{%s}\n", bbk.Title(p.Name), p.Name)
 	//fmt.Fprintf(os.Stdout, "{\\small Needs: %s}\n", strings.Join(p.Needs, ", "))
 	/*
 		for _, l := range p.Lines {
